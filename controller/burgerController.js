f94ba068db012533f54f475a44d16d5e7a6d0822
Adrian Romero <romeroaadrian@gmail.com>
2020-02-28 16:13:48 -0500

    second commit

--- controller/burgerController.js
+++ controller/burgerController.js
@@ -42,10 +42,10 @@ router.put("/api/burger/:id", function(req, res) {
   });
 });
 
-router.delete("/api/cats/:id", function(req, res) {
+router.delete("/api/burger/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  cat.delete(condition, function(result) {
+  burger.delete(condition, function(result) {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
